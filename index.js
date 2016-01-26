84a5a0c39a59c0294aa5fd500356e7b9f3938900
Eric Range <mail@eric-range.de>
2016-01-26 12:48:23 +0100

    Update

--- index.js
+++ index.js
@@ -1,38 +1,45 @@
 "use strict";
+var groups = {};
+var emit = (key, value) => {
+  if (!groups[key]) {
+    groups[key] = [];
+  }
+  groups[key].push(value);
+};
 
 var mapreduce = (docs, mapper, reducer) => {
-  let that = this;
-  that.groups = {};
-  emit(key, value) => {
-    if (!that.groups[key]) {
-      that.groups[key] = [];
-    }
-    that.groups[key].push(value);
-  }
-  docs.forEach((doc) {
-    mapper.call(doc, emit)
+  docs.forEach((doc) => {
+    mapper(doc);
   });
 
   let output = [];
-  Object.keys(that.groups).forEach((key) => {
-    output.push({
-      _id: key,
-      value: reducer(key, that.groups[key])
-    });
+  Object.keys(groups).forEach((key) => {
+    output.push(reducer(key, groups[key]));
   });
   return output;
 };
 
-let docs = [];
-let mapper = () => {
+var docs = require('./alles.json').metrics;
+var days = [2, 3, 4];
 
+let mapper = (doc) => {
+  let date = new Date(doc._id);
+  let day = date.getDay();
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+  let key = hour * 60 + minute;
+
+  let interval = 5;
+
+  if (days.indexOf(day) !== -1) {
+    emit(parseInt(key / interval), doc.net_sent);
+  }
 };
 
 let reducer = (key, values) => {
   let mean = values.reduce((a, b) => {
     return a + b;
   }) / values.length;
-
   return {
     key, mean
   };
